2917518769fb8ca252f1599a0924b7e55b20a4d0
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-22 12:12:01 +0000

    Automatically backed up by Learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,15 @@
 export default function createStore(reducer){
 // your code here!
 
-}
+    const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach( listener => listener() ) 
+    }
+
+    dispatch({})
+    return {
+      getState: getState,   
+      dispatch: dispatch,
+      subscribe: subscribe     
+      }
+  }
